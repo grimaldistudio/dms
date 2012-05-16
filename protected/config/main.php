9de7d5e5a67ef511b3ef6dd171bae842fd081ca6
engisolution <engisolution@engisolution-K53SD.(none)>
2012-05-16 08:45:01 +0200

    Disabled debug mode

--- protected/config/main.php
+++ protected/config/main.php
@@ -84,23 +84,15 @@ return array(
 			'charset' => 'utf8',
 		),
 		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-                        'errorAction'=>'site/error',
-                ),
+            'errorAction'=>'site/error',
+        ),
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
-
 				array(
 					'class'=>'CFileLogRoute',
 					'levels'=>'error, warning',
-				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
+				)
 			),
 		),
 		'format'=>array(

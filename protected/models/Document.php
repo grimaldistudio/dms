b5b8771d4c9137a4ac3a25c8ab0e210a421e65b0
root <root@imacdidomenico.station>
2015-07-08 18:06:49 +0200

    product release

--- protected/models/Document.php
+++ protected/models/Document.php
@@ -914,8 +914,8 @@ class Document extends CActiveRecord
     {
         $criteria=new CDbCriteria;
 
-        $criteria->compare('identifier',$this->identifier);
-        $criteria->compare('act_number',$this->act_number);
+        $criteria->compare('identifier',$this->identifier,true);
+        $criteria->compare('act_number',$this->act_number,true);
         $criteria->compare('name',$this->title,true);
        // $criteria->compare('main_document_type',$this->main_document_type);
         

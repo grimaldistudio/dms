470a010df775a35618b966464d23c6a428f1be35
root <root@imacdidomenico.station>
2015-07-09 18:12:33 +0200

    New contact page

--- protected/views/document/dashboard.php
+++ protected/views/document/dashboard.php
@@ -59,3 +59,74 @@
     ),
     'filter'=>$model
 )); ?>
+
+
+<?php
+Yii::app()->clientScript->registerScript('document-disable-controls', "
+    $('a.btn-enable').live('click', function(e)
+    {
+        e.preventDefault();
+        if(!confirm('Sei sicuro di voler ripubblicare il documento nell\'elenco?'))
+            return false;
+        var current_link = $(e.currentTarget);
+        $.ajax({
+            url: current_link.attr('href'),
+            method: 'POST',
+            cache: false,
+            dataType: 'json',
+            beforeSend: function()
+            {
+                current_link.addClass('disabled');
+            },
+            success: function(data)
+            {
+                if(data.success==1)
+                    $.fn.yiiListView.update('documentslistview');
+                else
+                    alert(data.errors.join(','));
+            },
+            error: function(xhr)
+            {
+                alert('Impossibile disabilitare il documento');
+            },
+            complete: function()
+            {
+                current_link.removeClass('disabled');            
+            }
+        });
+    });
+    
+    $('a.btn-delete').live('click', function(e)
+    {
+        e.preventDefault();
+        if(!confirm('Sei sicuro di voler cancellare definitivamente il documento?'))
+            return false;
+        var current_link = $(e.currentTarget);
+        $.ajax({
+            url: current_link.attr('href'),
+            method: 'POST',
+            cache: false,
+            dataType: 'json',
+            beforeSend: function()
+            {
+                current_link.addClass('disabled');
+            },
+            success: function(data)
+            {
+                if(data.success==1)
+                    $.fn.yiiListView.update('documentslistview');
+                else
+                    alert(data.errors.join(','));
+            },
+            error: function(xhr)
+            {
+                alert('Impossibile cancellare il documento');
+            },
+            complete: function()
+            {
+                current_link.removeClass('disabled');            
+            }
+        });
+    });
+");
+?>
